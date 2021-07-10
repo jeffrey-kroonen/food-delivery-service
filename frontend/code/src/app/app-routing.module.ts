ff901418c8a3cba3cdc03ebaef5e59061880c115
Jeffrey Kroonen <jeffreykroonen@gmail.com>
2021-07-10 09:48:02 +0200

    updated main html and css file

--- frontend/code/src/app/app-routing.module.ts
+++ frontend/code/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RestaurantOverviewComponent } from './components/restaurant-overview/restaurant-overview.component';
+import { RestaurantProductsComponent } from './components/restaurant-products/restaurant-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RestaurantOverviewComponent },
+  { path: 'restaurant/:id', component: RestaurantProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/code/src/app/app.module.ts
+++ frontend/code/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RestaurantOverviewComponent } from './components/restaurant-overview/restaurant-overview.component';
+import { RestaurantFiltersComponent } from './components/restaurant-filters/restaurant-filters.component';
+import { RestaurantProductsComponent } from './components/restaurant-products/restaurant-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    RestaurantOverviewComponent,
+    RestaurantFiltersComponent,
+    RestaurantProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
